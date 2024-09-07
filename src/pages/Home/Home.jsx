d5110bfbd236514f6a1c8f515e458b9ba40b023b
Jango <rafaeljangodev@gmail.com>
2024-09-07 20:25:54 -0300

    Sportify 1.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,18 +8,18 @@ const Home = () => {
         <div className="home-main-content">
           <h1>Encontre o melhor para o seu esporte!</h1>
           <p>
-            Na <span>Sportify</span>, temos tudo o que você precisa para se
-            destacar em qualquer modalidade. Equipamentos, roupas e acessórios
-            de alta qualidade, das maiores marcas, para te ajudar a alcançar o
-            seu melhor desempenho. Não importa se você é profissional ou
-            iniciante, aqui é o lugar certo para você!
+            Na Sportify, temos tudo o que você precisa para se destacar em
+            qualquer modalidade. Equipamentos, roupas e acessórios de alta
+            qualidade, das maiores marcas, para te ajudar a alcançar o seu
+            melhor desempenho. Não importa se você é profissional ou iniciante,
+            aqui é o lugar certo para você!
           </p>
           <div className="home-purchase-button">
             <a href="#">Comprar Agora</a>
           </div>
         </div>
 
-        <div className="home-banner">
+        <div className="home-banner2">
           <img
             src="https://images.unsplash.com/photo-1718638141977-3535fb151e51?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,8 +1,75 @@
-const Sobre = () => {
+import "../../styles/Sobre.css";
+
+const Home = () => {
   return (
     <>
-    </>
-  )
-}
+      <section className="home-sobre">
+        <div className="home-main-content">
+          <h1>
+            Sobre a <span>SPORTIFY</span>
+          </h1>
+          <p>
+            A Sportify é uma empresa comprometida em fornecer os melhores
+            produtos e serviços para atletas de todos os níveis. Nossas ideias
+            são baseadas na paixão pelo esporte e no desejo de ajudar os
+            praticantes a alcançarem seu máximo desempenho. Oferecemos uma ampla
+            variedade de equipamentos, roupas e acessórios de alta qualidade,
+            das melhores marcas do mercado, para garantir que você tenha tudo o
+            que precisa para se destacar em sua modalidade. Seja você um atleta
+            profissional ou um iniciante, a Sportify é o lugar certo para
+            encontrar o que você precisa!
+          </p>
+          <p>
+            Além disso, a Sportify também oferece serviços exclusivos para
+            ajudar os atletas a alcançarem seus objetivos. Nossa equipe de
+            profissionais qualificados está sempre pronta para oferecer
+            orientação e suporte personalizado, para que você possa se
+            desenvolver e evoluir em sua prática esportiva. Com a Sportify, você
+            terá todo o apoio necessário para alcançar o sucesso e se tornar o
+            melhor atleta que você pode ser!
+          </p>
+        </div>
 
-export default Sobre;
+        <div className="home-banner">
+          <img
+            src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+      </section>
+      <section className="home-sobre">
+        <div className="home-main-content1">
+          <p>
+            A <span>Sportify</span> é uma empresa comprometida em fornecer os
+            melhores produtos e serviços para atletas de todos os níveis. Nossas
+            ideias são baseadas na paixão pelo esporte e no desejo de ajudar os
+            praticantes a alcançarem seu máximo desempenho. Oferecemos uma ampla
+            variedade de equipamentos, roupas e acessórios de alta qualidade,
+            das melhores marcas do mercado, para garantir que você tenha tudo o
+            que precisa para se destacar em sua modalidade. Seja você um atleta
+            profissional ou um iniciante, a <span>Sportify</span> é o lugar
+            certo para encontrar o que você precisa!
+          </p>
+          <p>
+            Além disso, a <span>Sportify</span> também oferece serviços
+            exclusivos para ajudar os atletas a alcançarem seus objetivos. Nossa
+            equipe de profissionais qualificados está sempre pronta para
+            oferecer orientação e suporte personalizado, para que você possa se
+            desenvolver e evoluir em sua prática esportiva. Com a{" "}
+            <span>Sportify</span>, você terá todo o apoio necessário para
+            alcançar o sucesso e se tornar o melhor atleta que você pode ser!
+          </p>
+        </div>
+
+        <div className="home-banner">
+          <img
+            src="https://images.unsplash.com/photo-1523240795612-9a054b0db644?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
